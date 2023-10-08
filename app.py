32b2160c0cc19ae20549879b936cb5a4004dff89
Illia Liudogovskyi <Illia56@users.noreply.huggingface.co>
2023-10-08 20:02:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,9 @@ st.title("YouTube Video Chatbot")
 sidebar()
 initialize_session_state()
 
-if st.session_state.youtube_url:
+st.session_state.qa=None
+
+if st.session_state.youtube_url and st.session_state.qa==None:
     with st.status("Transcribing video..."):
       data = transcribe_video(st.session_state.youtube_url, PATH)
     
@@ -108,24 +110,23 @@ if st.session_state.youtube_url:
       retriever.search_kwargs['k'] = 4
     with st.status("Running RetrievalQA..."):
       llama_instance = LlamaLLM()
-      qa = RetrievalQA.from_chain_type(llm=llama_instance, chain_type="stuff", retriever=retriever)
+      st.session_state.qa = RetrievalQA.from_chain_type(llm=llama_instance, chain_type="stuff", retriever=retriever)
 
-    if "messages" not in st.session_state:
-      st.session_state.messages = []
+if "messages" not in st.session_state:
+  st.session_state.messages = []
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"], avatar=("🧑‍💻" if message["role"] == 'human' else '🦙')):
-            st.markdown(message["content"])
+for message in st.session_state.messages:
+    with st.chat_message(message["role"], avatar=("🧑‍💻" if message["role"] == 'human' else '🦙')):
+        st.markdown(message["content"])
 
-    textinput = st.chat_input("Ask LLama-2-70b anything about the video...")   
+textinput = st.chat_input("Ask LLama-2-70b anything about the video...") 
 
-    if prompt := textinput:
-      st.chat_message("human",avatar = "🧑‍💻").markdown(prompt)
-      st.session_state.messages.append({"role": "human", "content": prompt})
-
-      with st.status("Requesting Client..."):
-          response = qa.run(prompt)
-      with st.chat_message("assistant", avatar='🦙'):
-          st.markdown(response)
-      # Add assistant response to chat history
-      st.session_state.messages.append({"role": "assistant", "content": response})
+if prompt := textinput:
+  st.chat_message("human",avatar = "🧑‍💻").markdown(prompt)
+  st.session_state.messages.append({"role": "human", "content": prompt})
+  with st.status("Requesting Client..."):
+      response = st.session_state.qa.run(prompt)
+  with st.chat_message("assistant", avatar='🦙'):
+      st.markdown(response)
+  # Add assistant response to chat history
+  st.session_state.messages.append({"role": "assistant", "content": response})
